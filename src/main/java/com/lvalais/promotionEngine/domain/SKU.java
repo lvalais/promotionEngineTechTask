3e2443de6c8523626e3b7c109618d2fff44e2efd
Lampros Valais <lvalais@gmail.com>
2022-02-04 13:02:35 +0000

    JIRA-xxxx adding SKU object

---
+++ src/main/java/com/lvalais/promotionEngine/domain/SKU.java
@@ -0,0 +1,13 @@
+package com.lvalais.promotionEngine.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class SKU {
+    private String unitID;
+    private Integer price;
+}
