f01348cbc6a448c3be48bd57739df4dee43acfdd
Lampros Valais <lvalais@gmail.com>
2022-02-04 20:49:12 +0000

    JIRA-xxxx - Converting unit test class to demonstration of scenarios - adding separate unit test class for my unit test cases

---
+++ src/test/java/com/lvalais/promotionEngine/service/ProcessOrdersScenarioDemoTest.java
@@ -0,0 +1,73 @@
+package com.lvalais.promotionEngine.service;
+
+import com.lvalais.promotionEngine.domain.PromotionBase;
+import com.lvalais.promotionEngine.domain.SKUItem;
+import com.lvalais.promotionEngine.domain.SKUOrder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class ProcessOrdersScenarioDemoTest {
+    private ProcessOrders processOrders = new ProcessOrders();
+    private SKUOrder skuOrder;
+    private PromotionBase p1;
+    private PromotionBase p2;
+    private PromotionBase p3;
+
+    //below test cases mimic the scenarios in the requirements page.
+
+    @BeforeEach
+    void setUp() {
+        skuOrder = new SKUOrder();
+        //create promotion A
+        HashMap<String, Integer> promoHashMapA = new HashMap<>();
+        promoHashMapA.put("A",3);
+        p1 = new PromotionBase(1,promoHashMapA,130);
+        //create promotion B
+        HashMap<String, Integer> promoHashMapB = new HashMap<>();
+        promoHashMapB.put("B",2);
+        p2 = new PromotionBase(2,promoHashMapB,45);
+        //create promotion C
+        HashMap<String, Integer> promoHashMapC = new HashMap<>();
+        promoHashMapC.put("C",1);
+        promoHashMapC.put("D",1);
+        p3 = new PromotionBase(3,promoHashMapC,30);
+    }
+
+    @Test
+    public void processOrderTestScenarioA(){
+        List<SKUItem> skuList = new ArrayList<>();
+        skuList.add(new SKUItem("A",1, false,false));
+        skuList.add(new SKUItem("B",1,false,false));
+        skuList.add(new SKUItem("C",1,false,false));
+        skuOrder.setSkuListWithItemCount(skuList);
+        assertEquals(100,processOrders.processOrder(skuOrder, List.of(p1,p2,p3)));
+    }
+
+    @Test
+    public void processOrderTestScenarioB(){
+        List<SKUItem> skuList = new ArrayList<>();
+        skuList.add(new SKUItem("A",5, false,false));
+        skuList.add(new SKUItem("B",5,false,false));
+        skuList.add(new SKUItem("C",1,false,false));
+        skuOrder.setSkuListWithItemCount(skuList);
+        assertEquals(370,processOrders.processOrder(skuOrder, List.of(p1,p2,p3)));
+    }
+
+    @Test
+    public void processOrderTestScenarioC(){
+        List<SKUItem> skuList = new ArrayList<>();
+        skuList.add(new SKUItem("A",3, false,false));
+        skuList.add(new SKUItem("B",5,false,false));
+        skuList.add(new SKUItem("C",1,false,false));
+        skuList.add(new SKUItem("D",1,false,false));
+        skuOrder.setSkuListWithItemCount(skuList);
+        assertEquals(280,processOrders.processOrder(skuOrder, List.of(p1,p2,p3)));
+    }
+
+
+}
--- src/test/java/com/lvalais/promotionEngine/service/ProcessOrdersUnitTest.java
+++ src/test/java/com/lvalais/promotionEngine/service/ProcessOrdersUnitTest.java
@@ -1,25 +1,25 @@
 package com.lvalais.promotionEngine.service;
 
 import com.lvalais.promotionEngine.domain.PromotionBase;
-import com.lvalais.promotionEngine.domain.SKUBasePrice;
 import com.lvalais.promotionEngine.domain.SKUItem;
 import com.lvalais.promotionEngine.domain.SKUOrder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
-class ProcessOrdersTest {
+class ProcessOrdersUnitTest {
     private ProcessOrders processOrders = new ProcessOrders();
     private SKUOrder skuOrder;
-    private List<PromotionBase> activePromotions;
     private PromotionBase p1;
     private PromotionBase p2;
     private PromotionBase p3;
-
 
     @BeforeEach
     void setUp() {
@@ -28,39 +28,35 @@ class ProcessOrdersTest {
         HashMap<String, Integer> promoHashMapA = new HashMap<>();
         promoHashMapA.put("A",3);
         p1 = new PromotionBase(1,promoHashMapA,130);
-
         //create promotion B
         HashMap<String, Integer> promoHashMapB = new HashMap<>();
         promoHashMapB.put("B",2);
         p2 = new PromotionBase(2,promoHashMapB,45);
-
         //create promotion C
         HashMap<String, Integer> promoHashMapC = new HashMap<>();
         promoHashMapC.put("C",1);
         promoHashMapC.put("D",1);
         p3 = new PromotionBase(3,promoHashMapC,30);
-
     }
 
     @Test
-    public void processOrderTestScenarioA(){
+    public void processOrderTestScenarioTestNullPromoList(){
         List<SKUItem> skuList = new ArrayList<>();
         skuList.add(new SKUItem("A",1, false,false));
         skuList.add(new SKUItem("B",1,false,false));
         skuList.add(new SKUItem("C",1,false,false));
         skuOrder.setSkuListWithItemCount(skuList);
-        assertEquals(100,processOrders.processOrder(skuOrder, List.of(p1,p2,p3)));
+        assertEquals(100,processOrders.processOrder(skuOrder, null));
     }
 
     @Test
-    public void processOrderTestScenarioB(){
+    public void processOrderTestScenarioSKUItemWithZeroCount(){
         List<SKUItem> skuList = new ArrayList<>();
         skuList.add(new SKUItem("A",5, false,false));
         skuList.add(new SKUItem("B",5,false,false));
         skuList.add(new SKUItem("C",1,false,false));
+        skuList.add(new SKUItem("D",0,false,false));
         skuOrder.setSkuListWithItemCount(skuList);
-
-
         assertEquals(370,processOrders.processOrder(skuOrder, List.of(p1,p2,p3)));
     }
 
